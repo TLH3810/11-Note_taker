8798ea468c981422e7916b0b98efce28ee7efdcc
Yorkie Fan <terahowell@gmail.com>
2021-08-13 18:03:48 -0400

    updates to testing API routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,21 @@
 const router = require('express').Router();
-const dbNotes = require('../db/db.json')
+const dbNotes = require('../db/db.json');
+const fs = require('fs');
 
 //get notes from Database
 router.route("/notes")
     .get((req,res) => {
         console.log("GET/notes");
         res.status(200).send(data.notes);
+        return fs.readFile(dbNotes);
     })
     .post((req,res) => {
         console.log("POST/notes");
         console.log(req.body);
         const newNote = req.body;
         data.push(newNote);
-        res.json(newNote);
+        return res.json(newNote);
+//       fs.writeFile(dbNotes,JSON.stringify(newNote))
     })
 //    .delete((req,res) => {
         //console.log("DELETE/notes/:id");
